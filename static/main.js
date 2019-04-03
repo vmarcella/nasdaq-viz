c14211b64832ca980adba862f66ea136ca20b873
C3NZ <cenz@cenz.io>
2019-04-03 09:57:16 -0700

    [feat] generate ordinal colors, getCircleRadius, and other d3 setup items"

--- static/main.js
+++ static/main.js
@@ -5,5 +5,32 @@ $(function() {
 })
 
 function createGraph() {
-    //pass
+    const width = 960;
+    const height = 700;
+    const format = d3.format(",d"); // specify a decimal format that also allows values to be grouped by commas
+    // Color scale generated from i want hue 
+    const colors = d3.scaleOrdinal().range([
+        "#56c19e",
+        "#d34092",
+        "#49b94f",
+        "#a54fb7",
+        "#adb639",
+        "#7264d4",
+        "#86b75b",
+        "#da7cc9",
+        "#557e30",
+        "#5675c0",
+        "#d69d36",
+        "#4facd8",
+        "#c9522b",
+        "#37845f",
+        "#cd3e54",
+        "#8d7c38",
+        "#ac8dcd",
+        "#c78151",
+        "#9e4b6f",
+        "#db7a80"
+    ]);     
+    const circleRadius = d3.scalePow().domain([-100, 100]).range([-50, 50]);
+
 }
