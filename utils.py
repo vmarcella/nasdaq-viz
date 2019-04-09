89ba57b36e8d2d8660043f5a92fde15c57d99ff4
C3NZ <cenz@cenz.io>
2019-04-08 18:19:01 -0700

    [feat] add util file for working with pickled dataframes

---
+++ utils.py
@@ -0,0 +1,20 @@
+"""
+    Utility file for handling extraneous features
+"""
+import pickle
+
+import pandas as pd
+
+
+def load_dataframe(filepath):
+    """
+        Load a dataframe from a given file path
+    """
+    return pd.read_pickle(filepath)
+
+
+def save_dataframe(dataframe, filepath):
+    """
+        save a dataframe to a given file path
+    """
+    dataframe.to_pickle(filepath)
